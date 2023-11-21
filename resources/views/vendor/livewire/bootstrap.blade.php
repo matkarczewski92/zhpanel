08a8c9b3ecbc5e825fd273d909e7c68ec14a1961
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2023-11-21 08:25:10 +0100

    Update 1.2

---
+++ resources/views/vendor/livewire/bootstrap.blade.php
@@ -0,0 +1,62 @@
+@php
+if (! isset($scrollTo)) {
+    $scrollTo = '';
+}
+
+$scrollIntoViewJsSnippet = ($scrollTo !== false)
+    ? <<<JS
+       (\$el.closest('{$scrollTo}') || document.querySelector('{$scrollTo}')).scrollIntoView()
+    JS
+    : '';
+@endphp
+
+<div>
+    @if ($paginator->hasPages())
+        <div>
+            <ul class="pagination mt-4" style="display: flex;justify-content: right;">
+                {{-- Previous Page Link --}}
+                @if ($paginator->onFirstPage())
+                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+
+                    </li>
+                @else
+                    <li class="page-item">
+                        <button type="button"  style="border: none; color:white" dusk="previousPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="previousPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</button>
+                    </li>
+                @endif
+
+                {{-- Pagination Elements --}}
+                @foreach ($elements as $element)
+                    {{-- "Three Dots" Separator --}}
+                    @if (is_string($element))
+                        <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
+                    @endif
+
+                    {{-- Array Of Links --}}
+                    @if (is_array($element))
+                        @foreach ($element as $page => $url)
+                            @if ($page == $paginator->currentPage())
+                                <li class="page-item active"  wire:key="paginator-{{ $paginator->getPageName() }}-page-{{ $page }}" aria-current="page">
+                                    <span class="page-link" style="border-radius: 30%; border: none; background-color: rgb(146, 146, 146);">{{ $page }}</span></li>
+                            @else
+                                <li class="page-item" wire:key="paginator-{{ $paginator->getPageName() }}-page-{{ $page }}">
+                                    <button type="button" style="border: none; color:white" class="page-link" wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}">{{ $page }}</button></li>
+                            @endif
+                        @endforeach
+                    @endif
+                @endforeach
+
+                {{-- Next Page Link --}}
+                @if ($paginator->hasMorePages())
+                    <li class="page-item">
+                        <button type="button"  style="border: none; color:white"dusk="nextPage{{ $paginator->getPageName() == 'page' ? '' : '.' . $paginator->getPageName() }}" class="page-link" wire:click="nextPage('{{ $paginator->getPageName() }}')" x-on:click="{{ $scrollIntoViewJsSnippet }}" wire:loading.attr="disabled" rel="next" aria-label="@lang('pagination.next')">&rsaquo;</button>
+                    </li>
+                @else
+                    <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+
+                    </li>
+                @endif
+            </ul>
+        </div>
+    @endif
+</div>
