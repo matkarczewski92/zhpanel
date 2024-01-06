713b8500b77fe3e96aee2d797bfae1b006e5c9e6
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-01-06 12:18:06 +0100

    feature: add filtering options in litters planning view

--- resources/views/litters/components/litters-planning-div.blade.php
+++ resources/views/litters/components/litters-planning-div.blade.php
@@ -10,8 +10,8 @@
         <div class="card bg-dark photobg rounded-1 h-100">
             <div class="card-body text-center text-success" style="">
             @foreach ($connection ?? '' as $con)
-                <a href="{{ route('litters.show', $con->id)}}"><i class="bi bi-person-circle"></i></a>,
-                <a href="?filter={{$con->id}}" data-toggle="tooltip" data-placement="top" title="{{$con->connection_date ?? $con->planned_connection_date }}">{{$con->litter_code}}</a>
+                <a href="{{ route('litters.show', $con->id)}}"><i class="bi bi-person-circle"></i></a>
+                <a href="?filter={{$con->id}}" data-toggle="tooltip" data-placement="top" title="{{$con->connection_date ?? $con->planned_connection_date }}">{{$con->litter_code}}</a>,
             @endforeach
             </div>
         </div>
@@ -23,8 +23,8 @@
             @php
                 $layingday = date('Y-m-d', strtotime($con->connection_date ?? $con->planned_connection_date.' + '.systemConfig('layingDuration').' day'));
             @endphp
-                <a href="{{ route('litters.show', $con->id)}}"><i class="bi bi-person-circle"></i></a>,
-                <a href="?filter={{$con->id}}" data-toggle="tooltip" data-placement="top" title="{{$layingday}}">{{$con->litter_code}}</a>
+                <a href="{{ route('litters.show', $con->id)}}"><i class="bi bi-person-circle"></i></a>
+                <a href="?filter={{$con->id}}" data-toggle="tooltip" data-placement="top" title="{{$layingday}}">{{$con->litter_code}}</a>,
             @endforeach
             </div>
         </div>
@@ -36,8 +36,8 @@
             @php
                 $hatchlingday = date('Y-m-d', strtotime($con->connection_date ?? $con->planned_connection_date.' + '.systemConfig('layingDuration') + systemConfig('hatchlingDuration').' day'));
             @endphp
-                <a href="{{ route('litters.show', $con->id)}}"><i class="bi bi-person-circle"></i></a>,
-                <a href="?filter={{$con->id}}" data-toggle="tooltip" data-placement="top" title="{{$hatchlingday}}">{{$con->litter_code}}</a>
+                <a href="{{ route('litters.show', $con->id)}}"><i class="bi bi-person-circle"></i></a>
+                <a href="?filter={{$con->id}}" data-toggle="tooltip" data-placement="top" title="{{$hatchlingday}}">{{$con->litter_code}}</a>,
             @endforeach
             </div>
         </div>
