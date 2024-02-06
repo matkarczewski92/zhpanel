2ff1da1e964938424ff07fa908a2d4bcaaaf5a7d
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-02-06 11:05:39 +0100

    feature: add sensors module migrations

--- database/migrations/2024_02_06_095427_sensors.php
+++ database/migrations/2024_02_06_095427_sensors.php
@@ -4,14 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        //
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->id();
+            $table->text('sensor_id');
+            $table->text('sensor_type');
+            $table->decimal('min', 5, 2);
+            $table->decimal('max', 5, 2);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,5 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
     }
 };
