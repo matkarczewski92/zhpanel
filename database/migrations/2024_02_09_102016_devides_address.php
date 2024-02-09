afbdc9798cebc2fbad8a162be16343e46364d885
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-02-09 11:21:12 +0100

    fix: migrations

---
+++ database/migrations/2024_02_09_102016_devides_address.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sensors', function (Blueprint $table) {
+            $table->string('address');
+        });
+        Schema::table('relays', function (Blueprint $table) {
+            $table->string('address');
+        });
+        Schema::table('lights', function (Blueprint $table) {
+            $table->string('address');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+    }
+};
