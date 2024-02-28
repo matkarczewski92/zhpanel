6d9e33cede821ad0d24d238c0cfd71a5332bcaaf
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-02-28 10:46:10 +0100

    fix: correct slideshow arrow style

--- resources/views/webside/gallery.blade.php
+++ resources/views/webside/gallery.blade.php
@@ -20,11 +20,11 @@
 
             </div>
             <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <i class="bi bi-caret-left-fill h2"></i>
               <span class="visually-hidden">Previous</span>
             </button>
             <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <i class="bi bi-caret-right-fill h2"></i>
               <span class="visually-hidden">Next</span>
             </button>
           </div>
