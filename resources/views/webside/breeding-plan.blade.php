a6434269a8b0721fda9feef7192d361d6aaea257
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-03-05 11:36:54 +0100

    feature: breeding plan in main webside

---
+++ resources/views/webside/breeding-plan.blade.php
@@ -0,0 +1,17 @@
+<section>
+    <div class="parallax-six" id="offer">
+      <h2>PLANY HODOWLANE</h2>
+    </div>
+  </section>
+
+  <section>
+      <div class="block text-center"  style="min-height: 300px">
+          <h3 class="text-center mb-5">Plany hodowlane na bieżący rok  </h3>
+        @foreach ($litterPlans as $lP)
+            <p>{!! $lP->animalMale->name !!}  <i class="bi bi-arrow-through-heart-fill h4 @if($lP->category == 1) text-danger @endif"></i> {!! $lP->animalFemale->name !!}
+            @if($lP->category == 1) <i class="bi bi-check-lg h4 text-success"></i> @else <i class="bi bi-question-lg h4"></i> @endif</p>
+        @endforeach
+
+      </div>
+  </section>
+
