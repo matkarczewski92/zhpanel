05575a84126fc5d2f3d02d4fc9fb713c494d89fa
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-03-05 11:55:17 +0100

    feature: breeding plan in main webside

--- resources/views/webside/navbar.blade.php
+++ resources/views/webside/navbar.blade.php
@@ -13,6 +13,9 @@
             <a class="nav-link" href="#gallery">Galeria</a>
         </li>
         <li class="nav-item">
+            <a class="nav-link" href="#plans">Plany hodowlane</a>
+        </li>
+        <li class="nav-item">
             <a class="nav-link" href="#offer">Oferta</a>
         </li>
         <li class="nav-item">
@@ -26,9 +29,9 @@
         @if (Route::has('login'))
         <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
             @auth
-                <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ZH</a>
             @else
-                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">ZH</a>
 
                 {{-- @if (Route::has('register'))
                     <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
