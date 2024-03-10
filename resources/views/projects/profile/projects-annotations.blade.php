b60cfc0977162e09149de2f3b94429970df650a1
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-03-10 11:18:46 +0100

    feature: add annotation in project profile

--- resources/views/projects/profile/projects-annotations.blade.php
+++ resources/views/projects/profile/projects-annotations.blade.php
@@ -1,6 +1,6 @@
 <div class="card mb-5 me-4 ms-4 bg-dark photobg rounded-1 h-100">
     <div class="card-body " style="">
-        {{$project->annotations->id}}
+
         @if (is_null($project->annotations))
             <form action="{{ route('project.annotations.store', ['project' => $project]) }}" method="post">
                 @csrf
@@ -15,7 +15,7 @@
         <div class="strike mb-2">
             <span>Adnotacje</span>
          </div>
-         <textarea class="adnotations h-100" name="annotations" rows="6">{{ $project->annotations->annotations }}</textarea>
+         <textarea class="adnotations h-100" name="annotations" rows="6">{{ $project->annotations?->annotations ?? '' }}</textarea>
         </form>
          @if (Session::get('status') ?? '' == 'ok')
          <p class="text-center text-success">Zapisano pomyślnie</p>
