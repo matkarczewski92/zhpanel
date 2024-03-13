15ca47023786a58a78003b23aa1fd3007db06d07
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-03-13 17:09:31 +0100

    0x23: cookies

--- vendor/spatie/laravel-cookie-consent/resources/views/dialogContents.blade.php
+++ vendor/spatie/laravel-cookie-consent/resources/views/dialogContents.blade.php
@@ -1,8 +1,9 @@
 <!-- page content -->
-<div class="container py-4 mt-5">
+<div class="container py-4 mt-5 js-cookie-consent cookie-consent">
     <h1 class="font-weight-light"> Ciaaaaasteczka </h1>
     <p>{!! trans('cookie-consent::texts.message') !!}</p>
-    <button type="button" class="btn btn-outline-light mr-3" id="btnReset">
+    <button class="js-cookie-consent-agree cookie-consent__agree cursor-pointer flex items-center justify-center px-4 py-2 btn btn-danger">
         {{ trans('cookie-consent::texts.agree') }}
     </button>
 </div>
+
