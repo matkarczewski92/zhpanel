9a4481dd896c7c55d6b1855ced0677ec77a9ae51
matkarczewski92 <65907188+matkarczewski92@users.noreply.github.com>
2024-05-19 08:47:07 +0200

    fix: breeding plans display on main webpage

--- resources/views/webside/breeding-plan.blade.php
+++ resources/views/webside/breeding-plan.blade.php
@@ -8,10 +8,12 @@
       <div class="block text-center"  style="min-height: 300px">
           <h3 class="text-center mb-5">Plany hodowlane na bieżący rok  </h3>
         @foreach ($litterPlans as $lP)
-            <p>{!! $lP->animalMale->name !!}  <i class="bi bi-arrow-through-heart-fill h4 @if($lP->category == 1) text-danger @endif"></i> {!! $lP->animalFemale->name !!}
-            -
-            (<span>{{ $litterRepo->litterStatus($lP->id) }}</span>)
-        </p>
+            @if($lP->litter_code != 'PLAN')
+                <p>{!! $lP->animalMale->name !!}  <i class="bi bi-arrow-through-heart-fill h4 @if($lP->category == 1) text-danger @endif"></i> {!! $lP->animalFemale->name !!}
+                -
+                (<span>{{ $litterRepo->litterStatus($lP->id) }}</span>)
+                </p>
+            @endif
 
         @endforeach
 
