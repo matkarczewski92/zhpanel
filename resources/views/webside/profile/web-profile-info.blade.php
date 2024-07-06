9ff6ab83b5cbf62098eb93007dd4daa45aa55228
Mateusz <matkarczewski92@gmail.com>
2024-07-06 08:51:46 +0200

    add reservation info

--- resources/views/webside/profile/web-profile-info.blade.php
+++ resources/views/webside/profile/web-profile-info.blade.php
@@ -2,7 +2,7 @@
     <div class="card mb-3 bg-dark photobg rounded-1">
         <div class="card-body " style="">
 
-            <p class="text-center">Każdy wąż opuszcza naszą hodowlę w pojemniku hodowlanym, który umożliwia bezpieczny transport z hodowli do domu. 
+            <p class="text-center mt-2">Każdy wąż opuszcza naszą hodowlę w pojemniku hodowlanym, który umożliwia bezpieczny transport z hodowli do domu. 
                 <br/> Dodatkowo do każdego węża dołączamy mini poradnik, w którym zawarliśmy najważniejsze informacji dla początkującego hodowcy. </p>
             
        </div>
