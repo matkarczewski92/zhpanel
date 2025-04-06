6845e8654d74171d47ceb1b157e14e614718d76e
Mateusz <matkarczewski92@gmail.com>
2025-04-06 18:39:21 +0200

    display animal id

--- resources/views/litters/litters-list.blade.php
+++ resources/views/litters/litters-list.blade.php
@@ -3,7 +3,7 @@
         <td style="width:10%">Kod miotu</td>
         <td style="width:10%">Sezon</td>
         <td style="width:10%">Data łączenia</td>
-        <td style="width:10%">@if($d->laying_date) Data zniosu @else Data klucia @endif</td>
+        {{-- <td style="width:10%">@if($d->laying_date) Data zniosu @else Data klucia @endif</td> --}}
         <td>Samiec</td>
         <td>Samica</td>
         <td style="width:10%">Status</td>
@@ -21,7 +21,7 @@
         <td><a href="{{ route('litters.show', $d->id)}}">{{ $d->litter_code }}</a></td>
         <td>{{ $d->season }}</td>
         <td class="@if(!$d->connection_date) text-secondary @endif">{{ $d->connection_date ?? $d->planned_connection_date }}</td>
-        <td class="text-secondary">@if(!$d->laying_date) {{$d->connection_date}} @endif</td>
+        {{-- <td class="text-secondary">@if(!$d->laying_date) {{$d->connection_date}} @endif</td> --}}
         <td><a href="{{ route('animal.profile', $d->parent_male)}}">{!! $d->animalMale->name !!}</td>
         <td><a href="{{ route('animal.profile', $d->parent_female)}}">{!! $d->animalFemale->name !!}</td>
         <td>{{ $litterRepo->litterStatus($d->id) }}</td>
