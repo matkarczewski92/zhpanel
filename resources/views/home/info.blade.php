df33edd0e713973ebbed543d4b9f5a92a79d00e2
Mateusz <matkarczewski92@gmail.com>
2025-05-28 11:44:43 +0200

    t

--- resources/views/home/info.blade.php
+++ resources/views/home/info.blade.php
@@ -2,24 +2,24 @@
     <div class="col">
         <div class="card bg-dark photobg rounded-1" >
             <div class="text-center">
-                <h6 class="m-2">{{ $summary['eggs_count'] }}</h5>
-                <h5 class="m-2">Ilość jaj w inkubacji</h5>
+                <h5 class="m-2 mt-2">{{ $summary['eggs_count'] }}</h5>
+                <h6 class="m-2">Ilość jaj w inkubacji</h5>
             </div>
         </div>
     </div>
     <div class="col">
         <div class="card bg-dark photobg rounded-1" >
             <div class="text-center">
-                <h6 class="m-2">{{ $summary['for_sale'] }}</h5>
-                <h5 class="m-2">Ilość maluchów na sprzedaż</h5>
+                <h5 class="m-2 mt-2">{{ $summary['for_sale'] }}</h5>
+                <h6 class="m-2">Ilość maluchów na sprzedaż</h5>
             </div>
         </div>
     </div>
     <div class="col">
         <div class="card bg-dark photobg rounded-1" >
             <div class="text-center">
-                <h6 class="m-2">{{ $summary['litter_count'] }}</h5>
-                <h5 class="m-2">Aktualna ilość miotów</h5>
+                <h5 class="m-2 mt-2">{{ $summary['litter_count'] }}</h5>
+                <h6 class="m-2">Aktualna ilość miotów</h5>
             </div>
         </div>
     </div>
