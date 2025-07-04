8ec6135656f75ec82c3ed4c5d4d5289fc5422699
Mateusz <matkarczewski92@gmail.com>
2025-07-04 22:13:45 +0200

    f

--- resources/views/webside/profile/web-profile-offer.blade.php
+++ resources/views/webside/profile/web-profile-offer.blade.php
@@ -1,7 +1,7 @@
 <div class="card mb-3 bg-dark photobg rounded-1">
     <div class="card-body " style="">
         <div class="strike mb-2">
-            <span></span>
+            <span>Do wydania</span>
         </div>
         @if (session()->has('animalOffer'))
         <div class="alert alert-{{ session('animalOfferColor') }} alert-dismissible fade show" role="alert">
@@ -13,6 +13,7 @@
                 <tr>
                     <td class="key">Wartość hodowlana</td>
                     <td>{{ $animal->animalOffer->price ?? '' }} zł</td>
+                    
                 </tr>
                 {{-- <tr>
                     <td class="key">Data wystawienia</td>
@@ -27,7 +28,7 @@
                     <td>{{ $animal->animalOffer?->sold_date }}</td>
                 </tr> --}}
             </table>
-
+            <p class="text-center">Osobniki prezentowane na stronie nie stanowią oferty handlowej w rozumieniu art. 66 Kodeksu cywilnego.</p>
             @if(!is_null($animal->animalOffer?->offerReservation?->booker))
                 <div class="strike mb-2 text-danger">
                     <span>Rezerwacja</span>
