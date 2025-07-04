401b939c51ea204d4a9f815708636a7d10fc45ce
Mateusz <matkarczewski92@gmail.com>
2025-07-04 22:09:20 +0200

    f

--- resources/views/webside/for-sale.blade.php
+++ resources/views/webside/for-sale.blade.php
@@ -8,6 +8,7 @@
       <div class="block" style="min-height: 300px">
           <h3 class="text-center mb-5">Nadwyżki hodowlane</h3>
           <h3 class="text-center mb-3">osobniki wyłączone z dalszych planów hodowlanych – dostępne do przekazania pasjonatom</h3>
+          <br/>
 
           @if (!empty($offers[0]))
           <div class="row row-cols-1 row-cols-lg-5 g-4">
@@ -24,5 +25,8 @@
       <p class="text-center fw-bold">Nie wydajemy zwierząt osobom nieletnim.</p>
       <p class="text-center">Nie wysyłamy węży kurierem czy przesyłką konduktorską.
       <p class="text-center mb-4">Osobniki prezentowane na stronie nie stanowią oferty handlowej w rozumieniu art. 66 Kodeksu cywilnego.</p>
+      <p class="text-center mb-4">Prezentowane w profilach informacje mają charakter dokumentacyjny i nie są ofertą w rozumieniu przepisów prawa cywilnego ani handlowego. Ewentualne przekazanie osobników odbywa się wyłącznie na zasadzie prywatnego uzgodnienia, bez intencji zarobkowej.</p>
+      
+
   </section>
 
