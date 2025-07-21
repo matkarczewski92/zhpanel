d3261bdebc46b4dbdc8ce5726cdde9dde2d10a07
Mateusz <matkarczewski92@gmail.com>
2025-07-21 18:01:03 +0200

    g

--- resources/views/webside/for-sale.blade.php
+++ resources/views/webside/for-sale.blade.php
@@ -16,7 +16,7 @@
                 $litter = $firstOffer->animalDetails->litter ?? null;
             @endphp
                        
-                <h3 class="mt-5 mb-4"> <u>{{ $firstOffer->animalDetails->animalLitter->litter_code ?? '' }}</u> 
+                <h3 class="mt-5 mb-4"> <u>{{ $firstOffer->animalDetails->animalLitter->litter_code ?? 'Redukcja hodowli' }}</u> 
                   {!! $firstOffer->animalDetails->animalLitter->animalMale->name ?? '' !!} x 
                   {!! $firstOffer->animalDetails->animalLitter->animalFemale->name ?? '' !!} </h3>
 
