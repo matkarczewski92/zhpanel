00f4e7f02159f2379556b464e6870609bd2b130c
Mateusz <matkarczewski92@gmail.com>
2025-07-24 22:02:40 +0200

    g

--- resources/views/webside/for-sale.blade.php
+++ resources/views/webside/for-sale.blade.php
@@ -19,7 +19,7 @@
                 <h3 class="mt-5 mb-4"> <u>{{ $firstOffer->animalDetails->animalLitter->litter_code ?? 'Redukcja hodowli' }}</u> </h3> 
                 <h3 class="mt-5 mb-3">
                   {!! $firstOffer->animalDetails->animalLitter->animalMale->name ?? '' !!} </h3> 
-                <h3 class="mt-3 mb-4"> 
+                <h3 class="mt-4 mb-4"> 
                   {!! $firstOffer->animalDetails->animalLitter->animalFemale->name ?? '' !!} </h3>
 
                 <div class="row row-cols-1 row-cols-lg-5 g-4">
