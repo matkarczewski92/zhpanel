9a48142c90d775df1e35e28ecf9ae63588440edc
Mateusz <matkarczewski92@gmail.com>
2025-07-25 17:54:02 +0200

    g

--- resources/views/webside/profile/web-profile-photo.blade.php
+++ resources/views/webside/profile/web-profile-photo.blade.php
@@ -18,3 +18,16 @@
      </div>
    </div>
  </div>
+<div class="modal fade" id="photo_profil" tabindex="-1" aria-labelledby="photo_profil" aria-hidden="true">
+   <div class="modal-dialog modal-xl ">
+     <div class="modal-content bg-dark photobg">
+       <div class="modal-header border-0" >
+           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+         </div>
+       <div class="modal-body" style="top: -25px">
+           <a href="#" data-bs-dismiss="modal"><img src="{{ $animal->animalMainPhoto?->url }}" class="img-fluid " alt=""></a>
+       </div>
+
+     </div>
+   </div>
+ </div>
