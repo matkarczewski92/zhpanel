97d85efb266914f17602b54538caf70a41d1acb1
Mateusz <matkarczewski92@gmail.com>
2025-08-13 17:31:32 +0200

    d

--- resources/views/livewire/litters/fb.blade.php
+++ resources/views/livewire/litters/fb.blade.php
@@ -1,13 +1,53 @@
 <div class="card mb-3 bg-dark photobg rounded-1 h-100">
-    <div class="card-body " style="">
-        <button type="button" wire:click="save()" class="btn btn-success rounded-circle editmode">
-            <i class="fa-regular fa-floppy-disk fa-lg"></i>
-        </button>
-        <div class="strike mb-2">
-            <span>Publikuj post na FB</span>
-         </div>
-         <textarea class="adnotations h-75" wire:model="adnotation"></textarea>
+    <div class="card-body">
+
+        {{-- LOADER przy wysyłaniu --}}
+        <div wire:loading.inline>
+            <div class="alert alert-secondary mb-3">
+                Wysyłanie… Proszę czekać.
+            </div>
+        </div>
+
+        {{-- JEŚLI NIE WYSŁANO – FORMULARZ --}}
+        @unless($submitted)
+            <div class="strike mb-2"><span>Publikuj post na FB</span></div>
+
+            <textarea class="adnotations form-control mb-3"
+                      rows="8"
+                      wire:model.defer="adnotation"></textarea>
+
+            <button type="button"
+                    wire:click="save"
+                    wire:loading.attr="disabled"
+                    class="btn btn-primary">
+                Zapisz i wyślij
+            </button>
+        @endunless
+
+        {{-- JEŚLI WYSŁANO – PODSUMOWANIE --}}
+        @if($submitted)
+            @if($apiOk)
+                <div class="alert alert-success">
+                    <strong>Wysłano!</strong> Post został opublikowany.
+                </div>
+
+                @if($apiPayload)
+                    <div class="card bg-black border-secondary mb-3">
+                        <div class="card-header">Odpowiedź API</div>
+                        <div class="card-body">
+<pre class="mb-0 text-light" style="white-space: pre-wrap;">{{ json_encode($apiPayload, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE) }}</pre>
+                        </div>
+                    </div>
+                @endif
+            @else
+                <div class="alert alert-danger">
+                    <strong>Błąd!</strong> {{ $error ?? 'Nieznany błąd.' }}
+                </div>
+            @endif
+
+            <button type="button" class="btn btn-outline-light" wire:click="resetForm">
+                Wyślij kolejny post
+            </button>
+        @endif
     </div>
 </div>
-
- 
