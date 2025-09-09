67b21a199facac5fb6c6e862a9981e9f983475d3
Mateusz <matkarczewski92@gmail.com>
2025-09-09 09:52:03 +0200

    f

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="_token" content="{{ csrf_token() }}">
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <script src="https://kit.fontawesome.com/c530abb7f2.js" crossorigin="anonymous"></script>
     <script src="//code.jquery.com/jquery-1.12.3.js"></script>
--- resources/views/layouts/guests.blade.php
+++ resources/views/layouts/guests.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=0.8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
     <script src="https://kit.fontawesome.com/c530abb7f2.js" crossorigin="anonymous"></script>
     <script src="//code.jquery.com/jquery-1.12.3.js"></script>
