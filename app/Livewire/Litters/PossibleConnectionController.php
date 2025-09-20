9131a6efcc555efde6af29eb0d7b8afc0d127356
Mateusz <matkarczewski92@gmail.com>
2025-09-20 13:16:03 +0200

    t

--- app/Livewire/Litters/PossibleConnectionController.php
+++ app/Livewire/Litters/PossibleConnectionController.php
@@ -44,7 +44,7 @@ class PossibleConnectionController extends Component
             $possibleCombinations = [];
             $fm = $this->animalRepo->getById($femaleId);
             $female = $this->getAnimalArray($this->animalRepo->getById($femaleId));
-            $males = $this->animalRepo->getAllInBreedingMales()->where('animal_category_id', $fm->animal_category_id);
+            $males = $this->animalRepo->getAllInBreedingMales()->where('animal_type_id', $fm->animal_type_id);
             dd($males);
             foreach ($males as $male) {
             $maleObj = $this->getAnimalArray($male);
