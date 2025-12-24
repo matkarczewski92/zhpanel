b268c17952a973a57494b864a8d811d1b05079a3
Mateusz <matkarczewski92@gmail.com>
2025-12-24 09:01:09 +0100

    f

--- resources/views/livewire/litters/possible-connection-controller.blade.php
+++ resources/views/livewire/litters/possible-connection-controller.blade.php
@@ -203,8 +203,9 @@
                                                 $fId = (int) $pair['female_id'];
                                                 $mId = (int) $pair['male_id'];
                                                 $offspringRows = $this->getPairRows($fId, $mId);
+                                                dd($mId);
                                                 $femaleName = $femalesMap[$fId] ?? ('ID: '.$fId);
-                                                $maleName   = $malesMap[$mId]   ;
+                                                $maleName   = $malesMap[$mId] ?? ('ID: '.$mId);
                                             @endphp
 
                                             <tr>
